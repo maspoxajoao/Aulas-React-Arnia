ac995bd8bb0c2d50ae30adb7122d9a25a4609fd9
João Victor <116379577+maspoxajoao@users.noreply.github.com>
2023-05-04 22:58:00 -0300

    aula finalizada

--- Aula Pratica/-Vite-project/src/main.jsx
+++ Aula Pratica/-Vite-project/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { HelloWorld, HelloWorld2 } from './components/HelloWolrd'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <HelloWorld/>
+        <HelloWorld2/>
   </React.StrictMode>,
 )
